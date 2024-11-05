6a6bbd36678fcce8aaba59301a386453e27360c3
Sklenator <sklenator@seznam.cz>
2024-11-05 14:05:33 +0100

    Upgraded Android Gradle Plugin version from 8.7.0 to 8.7.2, updated current version from "Release 1.0 to Alpha 0.1", updated safeArgs from 2.7.2 to 2.8.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ buildscript {
         google()
     }
     dependencies {
-        val navVersion = "2.7.2"
+        val navVersion = "2.8.3"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
 
 
 plugins {
-    id("com.android.application") version "8.7.0" apply false
+    id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
